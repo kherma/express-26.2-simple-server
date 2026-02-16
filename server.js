c9049d99675d723816d030ed2b9ca21515acf8ba
kherma <kamil.herma01@gmail.com>
2026-02-16 22:31:16 +0100

    feat: add handlebars rendering engin and templates

--- server.js
+++ server.js
@@ -1,25 +1,21 @@
 const path = require('path');
 const express = require('express');
+const hbs = require('express-handlebars');
 const app = express();
 const port = process.env.PORT || 3000;
+
+app.engine('.hbs', hbs());
+app.set('view engine', '.hbs');
 
 const publicDir = path.join(__dirname, 'public');
 
 app.use(express.static(publicDir));
 
-// Middleware to provide `res.show(name)` for serving files from /views
-app.use((req, res, next) => {
-  res.show = (name) => {
-    return res.sendFile(path.join(__dirname, 'views', name));
-  };
-  next();
-});
-
 function requireLogin(req, res, next) {
   const loggedIn = false;
 
   if (!loggedIn) {
-    return res.status(401).show('login-required.html');
+    res.status(401).render('login-required', { layout: false });
   }
 
   next();
@@ -29,21 +25,20 @@ function requireLogin(req, res, next) {
 app.use('/user', requireLogin);
 
 app.get(['/', '/home'], (req, res) => {
-  return res.show('hello.html');
+  res.render('hello', { layout: false });
 });
 
 app.get('/about', (req, res) => {
-  return res.show('about.html');
+  res.render('about', { layout: false });
 });
 
 app.get('/hello/:name', (req, res) => {
-  return res.send(`Hello ${req.params.name}`);
+  res.render('hello', { layout: false, name: req.params.name });
 });
 
 // Catch-all 404: send local 404 page with image
 app.use((req, res) => {
-  res.status(404);
-  return res.show('404.html');
+  res.status(404).render('404', { layout: false });
 });
 
 app.listen(port, () => {
