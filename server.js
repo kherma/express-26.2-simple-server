d32ee8097209d799f27cbb7342532923ec840909
kherma <kamil.herma01@gmail.com>
2026-02-16 22:43:00 +0100

    feat: add handlebars main layout

--- server.js
+++ server.js
@@ -4,7 +4,11 @@ const hbs = require('express-handlebars');
 const app = express();
 const port = process.env.PORT || 3000;
 
-app.engine('.hbs', hbs());
+app.engine(
+  'hbs',
+  hbs({ extname: 'hbs', layoutsDir: './layouts', defaultLayout: 'main' }),
+);
+
 app.set('view engine', '.hbs');
 
 const publicDir = path.join(__dirname, 'public');
@@ -15,7 +19,7 @@ function requireLogin(req, res, next) {
   const loggedIn = false;
 
   if (!loggedIn) {
-    res.status(401).render('login-required', { layout: false });
+    res.status(401).render('login-required');
   }
 
   next();
@@ -25,11 +29,22 @@ function requireLogin(req, res, next) {
 app.use('/user', requireLogin);
 
 app.get(['/', '/home'], (req, res) => {
-  res.render('hello', { layout: false });
+  res.render('hello');
 });
 
 app.get('/about', (req, res) => {
-  res.render('about', { layout: false });
+  res.render('about');
+});
+
+app.get('/contact', (req, res) => {
+  res.render('contact');
+});
+
+app.get('/info', (req, res) => {
+  res.render('info');
+});
+app.get('/history', (req, res) => {
+  res.render('history');
 });
 
 app.get('/hello/:name', (req, res) => {
@@ -38,7 +53,7 @@ app.get('/hello/:name', (req, res) => {
 
 // Catch-all 404: send local 404 page with image
 app.use((req, res) => {
-  res.status(404).render('404', { layout: false });
+  res.status(404).render('404');
 });
 
 app.listen(port, () => {
